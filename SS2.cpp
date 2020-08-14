9eee9ca67de8ea58f0eeb08b0d9cc13b840ae217
Ryzen5 <zenkikuchi3007@gmail.com>
2020-08-15 06:31:27 +0900

    Setting and Checking

--- SS2.cpp
+++ SS2.cpp
@@ -2,10 +2,51 @@
 //
 
 #include <iostream>
-
+#include <opencv2\opencv.hpp>
+#include <MIDIData.h>
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+	MIDIData* pMIDIData;
+	MIDITrack* pMIDITrack;
+	/* MIDIデータの生成(フォーマット0,トラック数1,TPQNベース,120) */
+	pMIDIData = MIDIData_Create(MIDIDATA_FORMAT0, 1, MIDIDATA_TPQNBASE, 120);
+	if (pMIDIData == NULL) {
+		printf("MIDIデータの生成に失敗しました。\n");
+		return 0;
+	}
+	/* 最初のトラックへのポインタを取得 */
+	pMIDITrack = MIDIData_GetFirstTrack(pMIDIData);
+	/* イベントを挿入 */
+	MIDITrack_InsertTrackName(pMIDITrack, 0, L"ちょうちょ"); /* タイトル */
+	MIDITrack_InsertTempo(pMIDITrack, 0, 60000000 / 120); /* 120BPM */
+	MIDITrack_InsertProgramChange(pMIDITrack, 0, 0, 0); /* Piano1 */
+	/* ノートイベントを挿入            Time  ch  key  vel  dur */
+	MIDITrack_InsertNote(pMIDITrack, 0, 0, 67, 100, 120); /* そ */
+	MIDITrack_InsertNote(pMIDITrack, 120, 0, 64, 100, 120); /* み */
+	MIDITrack_InsertNote(pMIDITrack, 240, 0, 64, 100, 240); /* みー */
+	MIDITrack_InsertNote(pMIDITrack, 480, 0, 65, 100, 120); /* ふぁ */
+	MIDITrack_InsertNote(pMIDITrack, 600, 0, 62, 100, 120); /* れ */
+	MIDITrack_InsertNote(pMIDITrack, 720, 0, 62, 100, 240); /* れー */
+	MIDITrack_InsertNote(pMIDITrack, 960, 0, 60, 100, 120); /* ど */
+	MIDITrack_InsertNote(pMIDITrack, 1080, 0, 62, 100, 120); /* れ */
+	MIDITrack_InsertNote(pMIDITrack, 1200, 0, 64, 100, 120); /* み */
+	MIDITrack_InsertNote(pMIDITrack, 1320, 0, 65, 100, 120); /* ふぁ */
+	MIDITrack_InsertNote(pMIDITrack, 1440, 0, 67, 100, 120); /* そ */
+	MIDITrack_InsertNote(pMIDITrack, 1560, 0, 67, 100, 120); /* そ */
+	MIDITrack_InsertNote(pMIDITrack, 1680, 0, 67, 100, 240); /* そー */
+	/* エンドオブトラックイベントを挿入 */
+	MIDITrack_InsertEndofTrack(pMIDITrack, 1920);
+	/* MIDIデータを保存 */
+	MIDIData_SaveAsSMF(pMIDIData, L"chocho.mid");
+	/* MIDIデータをメモリ上から削除 */
+	MIDIData_Delete(pMIDIData);
+	pMIDIData = NULL;
+	cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
+	imshow("", image);
+	cv::waitKey(0);
+	char str;
+	std::cin >> str;
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
